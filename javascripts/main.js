76a717bcfac11fadb217c0337c0aefcbeea5fb40
chenghsuango@gmail.com <chenghsuango@gmail.com>
2015-05-01 23:57:05 +0800

    Automated commit at 2015-05-01 15:57:05 UTC by middleman-deploy 1.0.0

---
+++ javascripts/main.js
@@ -0,0 +1,7 @@
+$('.popup-btn').click(function(){
+	$(this).parent().parent('.popup-notification').addClass('close');
+})
+$('.group-leader-notice-btn').click(function(){
+	$('.black-mask').removeClass('active');
+})
+;

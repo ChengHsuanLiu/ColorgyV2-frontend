0ffad01c21197ea3efad336916869115834e9e75
ChengHsuanLiu <chenghsuango@gmail.com>
2015-05-01 23:06:52 +0800

    frontend update

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,3 +1,6 @@
 $('.popup-btn').click(function(){
 	$(this).parent().parent('.popup-notification').addClass('close');
 })
+$('.group-leader-notice-btn').click(function(){
+	$('.black-mask').removeClass('active');
+})

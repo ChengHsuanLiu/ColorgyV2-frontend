074ec5cbd2bfea3aa594ff700133ea0ec0dbbfb7
ChengHsuanLiu <chenghsuango@gmail.com>
2015-05-15 21:42:42 +0800

    front-end update

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -27,3 +27,8 @@ $('.slide-field-trigger').click(function(){
 	var targetName = triggerName.substring(1, triggerName.length);
 	$('#' + targetName).slideToggle(300);
 })
+
+$('.collapse-field-with-title .title').click(function(){
+	$(this).children('.trigger').toggleClass('active');
+	$(this).siblings('.content').slideToggle(300);
+})
